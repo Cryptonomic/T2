b5b5a65993024aff4f40d4e11de27ed66b2d1900
developer0623 <developer0623@yahoo.com>
2020-04-15 22:55:07 -0400

    implemented the sign-verify view

--- src/contracts/SignVerify/components/Verify.tsx
+++ src/contracts/SignVerify/components/Verify.tsx
@@ -1,27 +1,70 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
+import { TezosWalletUtil } from 'conseiljs';
+import Snackbar from '@material-ui/core/Snackbar';
+import Button from '@material-ui/core/Button';
 
-const Container = styled.div`
-  width: 100%;
-  padding: 0 20px 20px 20px;
-`;
+import CustomTextArea from '../../../components/CustomTextArea';
+import TextField from '../../../components/TextField';
+import InputAddress from '../../../components/InputAddress';
 
-const MainContainer = styled.div`
-  width: 100%;
-  margin-top: 30px;
-`;
+import { RootState } from '../../../types/store';
 
-const CodeTxt = styled.div`
-  font-size: 16px;
-  color: ${({ theme: { colors } }) => colors.primary};
-  word-wrap: break-word;
-  white-space: pre;
-`;
+import { Container, MainContainer, ButtonContainer, InvokeButton } from './style';
 
 const Verify = () => {
+  const { t } = useTranslation();
+  const isLoading = useSelector((rootState: RootState) => rootState.app.isLoading);
+  const [message, setMessage] = useState('');
+  const [signature, setSignature] = useState('');
+  const [address, setAddress] = useState('');
+  const [isAddressIssue, setIsAddressIssue] = useState(false);
+  const [result, setResult] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const title = result ? t('general.verbs.match') : t('general.verbs.no_match');
+
+  const isDisabled = isLoading || !message || !signature || !address || isAddressIssue;
+
+  async function onVerify() {
+    const isVerified = await TezosWalletUtil.checkSignature(signature, message, address);
+    setResult(isVerified);
+    setIsOpen(true);
+  }
+
   return (
     <Container>
-      <MainContainer/>
+      <MainContainer>
+        <CustomTextArea label={t('general.nouns.message')} onChange={val => setMessage(val)} />
+        <TextField label={t('general.nouns.signature')} onChange={val => setSignature(val)} />
+
+        <InputAddress
+          label={t('general.nouns.label_address')}
+          operationType="send_babylon"
+          tooltip={false}
+          onChange={val => setAddress(val)}
+          onIssue={val => setIsAddressIssue(val)}
+        />
+      </MainContainer>
+      <ButtonContainer>
+        <InvokeButton buttonTheme="primary" disabled={isDisabled} onClick={onVerify}>
+          {t('general.verbs.verify')}
+        </InvokeButton>
+      </ButtonContainer>
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center'
+        }}
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+        message={title}
+        action={
+          <Button color="secondary" size="small" onClick={() => setIsOpen(false)}>
+            {t('general.nouns.ok')}
+          </Button>
+        }
+      />
     </Container>
   );
 };
