197c9d8ef6298adafd8a04744b20e523688243c9
Darius <dariusz.sobkowicz@gmail.com>
2020-04-21 10:46:48 +0200

    register custom-url protocol

--- src/main.dev.js
+++ src/main.dev.js
@@ -1,26 +1,25 @@
 const electron = require('electron');
 const app = electron.app;
+const protocol = electron.protocol;
 const BrowserWindow = electron.BrowserWindow;
 
 let mainWindow = null;
 
 if (process.env.NODE_ENV === 'production') {
-  const sourceMapSupport = require('source-map-support');
-  sourceMapSupport.install();
+    const sourceMapSupport = require('source-map-support');
+    sourceMapSupport.install();
 }
 
 if (process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true') {
-  require('electron-debug')();
+    require('electron-debug')();
 }
 
 const installExtensions = async () => {
-  const installer = require('electron-devtools-installer');
-  const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
-  const extensions = ['REACT_DEVELOPER_TOOLS', 'REDUX_DEVTOOLS'];
+    const installer = require('electron-devtools-installer');
+    const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
+    const extensions = ['REACT_DEVELOPER_TOOLS', 'REDUX_DEVTOOLS'];
 
-  return Promise.all(
-    extensions.map(name => installer.default(installer[name], forceDownload))
-  ).catch();
+    return Promise.all(extensions.map(name => installer.default(installer[name], forceDownload))).catch();
 };
 
 /**
@@ -28,47 +27,52 @@ const installExtensions = async () => {
  */
 
 app.on('window-all-closed', () => {
-  // Respect the OSX convention of having the application in memory even
-  // after all windows have been closed
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    // Respect the OSX convention of having the application in memory even
+    // after all windows have been closed
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('ready', async () => {
-  if (process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true') {
-    await installExtensions();
-  }
-
-  mainWindow = new BrowserWindow({
-    height: 768,
-    minHeight: 768,
-    minWidth: 1024,
-    show: false,
-    title: 'Tezori',
-    webPreferences: {
-      nodeIntegration: true
-    },
-    width: 1120
-  });
-
-  mainWindow.loadURL(`file://${__dirname}/app.html`);
-
-  // @TODO: Use 'ready-to-show' event
-  //        https://github.com/electron/electron/blob/master/docs/api/browser-window.md#using-ready-to-show-event
-  mainWindow.webContents.on('did-finish-load', () => {
-    if (!mainWindow) {
-      throw new Error('"mainWindow" is not defined');
+    if (process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true') {
+        await installExtensions();
     }
-    if (process.env.START_MINIMIZED) {
-      mainWindow.minimize();
-    } else {
-      mainWindow.show();
-      mainWindow.focus();
-    }
-  });
 
-  mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
+    protocol.registerStringProtocol('custom-url', (request, callback) => {
+        const url = request.url.substr(11);
+        callback(url);
+    });
+
+    mainWindow = new BrowserWindow({
+        height: 768,
+        minHeight: 768,
+        minWidth: 1024,
+        show: false,
+        title: 'Tezori',
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width: 1120
+    });
+
+    mainWindow.loadURL(`file://${__dirname}/app.html`);
+
+    // @TODO: Use 'ready-to-show' event
+    //        https://github.com/electron/electron/blob/master/docs/api/browser-window.md#using-ready-to-show-event
+    mainWindow.webContents.on('did-finish-load', () => {
+        if (!mainWindow) {
+            throw new Error('"mainWindow" is not defined');
+        }
+        if (process.env.START_MINIMIZED) {
+            mainWindow.minimize();
+        } else {
+            mainWindow.show();
+            mainWindow.focus();
+        }
+    });
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
 });
