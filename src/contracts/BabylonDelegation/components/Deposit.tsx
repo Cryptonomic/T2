1346f334c40e04dd7fb4b784d8ee0c9275b864b0
anonymoussprocket <anonymoussprocket@gmail.com>
2020-04-21 23:17:42 -0400

    - default delegation contract fees

--- src/contracts/BabylonDelegation/components/Deposit.tsx
+++ src/contracts/BabylonDelegation/components/Deposit.tsx
@@ -20,157 +20,127 @@ import { RegularAddress } from '../../../types/general';
 
 import { depositThunk } from '../../duck/thunks';
 
-import {
-  Container,
-  AmountContainer,
-  FeeContainer,
-  UseMax,
-  PasswordButtonContainer,
-  InvokeButton,
-  WarningContainer,
-  InfoText
-} from './style';
+import { Container, AmountContainer, FeeContainer, UseMax, PasswordButtonContainer, InvokeButton, WarningContainer, InfoText } from './style';
 
 const utez = 1000000;
 
 interface Props {
-  isReady: boolean;
-  addresses: RegularAddress[];
-  onSuccess: () => void;
+    isReady: boolean;
+    addresses: RegularAddress[];
+    onSuccess: () => void;
 }
 
 function Deposit(props: Props) {
-  const { t } = useTranslation();
-  const dispatch = useDispatch();
-  const [fee, setFee] = useState(5000);
-  const [amount, setAmount] = useState('');
-  const [passPhrase, setPassPhrase] = useState('');
-  const [open, setOpen] = useState(false);
+    const { t } = useTranslation();
+    const dispatch = useDispatch();
+    const [fee, setFee] = useState(5000);
+    const [amount, setAmount] = useState('');
+    const [passPhrase, setPassPhrase] = useState('');
+    const [open, setOpen] = useState(false);
 
-  const { newFees, isFeeLoaded } = useFetchFees(OperationKindType.Transaction, false);
-  const { isLoading, isLedger, selectedAccountHash, selectedParentHash } = useSelector<
-    RootState,
-    AppState
-  >((state: RootState) => state.app, shallowEqual);
-  const { isReady, addresses, onSuccess } = props;
+    const { newFees } = useFetchFees(OperationKindType.Transaction, false);
+    const { isLoading, isLedger, selectedAccountHash, selectedParentHash } = useSelector<RootState, AppState>((state: RootState) => state.app, shallowEqual);
+    const { isReady, addresses, onSuccess } = props;
 
-  useEffect(() => {
-    setFee(newFees.medium);
-  }, [isFeeLoaded]);
-
-  function onGetMax() {
-    const max = addresses[0].balance - fee - 3_000_000;
-    const newAmount = max > 0 ? (max / utez).toFixed(6) : '0';
-    setAmount(newAmount);
-  }
-
-  function getBalanceState() {
-    const realAmount = !amount ? Number(amount) : 0;
-    const max = addresses[0].balance - fee - 3_000_000;
-
-    if (max <= 0 || max < realAmount) {
-      return {
-        isIssue: true,
-        warningMessage: t('components.send.warnings.total_exceeds')
-      };
+    function onGetMax() {
+        const max = addresses[0].balance - fee - 3_000_000;
+        const newAmount = max > 0 ? (max / utez).toFixed(6) : '0';
+        setAmount(newAmount);
     }
 
-    return {
-      isIssue: false,
-      warningMessage: ''
-    };
-  }
+    function getBalanceState() {
+        const realAmount = !amount ? Number(amount) : 0;
+        const max = addresses[0].balance - fee - 3_000_000;
 
-  async function onInvokeOperation() {
-    dispatch(setIsLoadingAction(true));
-    if (isLedger) {
-      setOpen(true);
+        if (max <= 0 || max < realAmount) {
+            return {
+                isIssue: true,
+                warningMessage: t('components.send.warnings.total_exceeds')
+            };
+        }
+
+        return {
+            isIssue: false,
+            warningMessage: ''
+        };
     }
 
-    const operationResult = await dispatch(
-      depositThunk(fee, amount, passPhrase, selectedAccountHash)
+    async function onInvokeOperation() {
+        dispatch(setIsLoadingAction(true));
+        if (isLedger) {
+            setOpen(true);
+        }
+
+        const operationResult = await dispatch(depositThunk(fee, amount, passPhrase, selectedAccountHash));
+
+        setOpen(false);
+        dispatch(setIsLoadingAction(false));
+
+        if (!!operationResult) {
+            onSuccess();
+        }
+    }
+
+    function onEnterPress(keyVal, disabled) {
+        if (keyVal === 'Enter' && !disabled) {
+            onInvokeOperation();
+        }
+    }
+
+    const { isIssue, warningMessage } = getBalanceState();
+    const isDisabled = !isReady || isLoading || isIssue || amount === '0' || !amount || (!passPhrase && !isLedger);
+
+    const error = isIssue ? <InputError error={warningMessage} /> : '';
+
+    const warningTxt = t('components.withdrawDeposit.deposit_warning', {
+        managerAddress: selectedParentHash
+    });
+
+    return (
+        <Container onKeyDown={event => onEnterPress(event.key, isDisabled)}>
+            <AmountContainer>
+                <TezosNumericInput
+                    decimalSeparator={t('general.decimal_separator')}
+                    label={t('general.nouns.amount')}
+                    amount={amount}
+                    onChange={val => setAmount(val)}
+                    errorText={error}
+                />
+                <UseMax onClick={onGetMax}>{t('general.verbs.use_max')}</UseMax>
+            </AmountContainer>
+            <FeeContainer>
+                <Fees low={newFees.low} medium={newFees.medium} high={newFees.high} fee={fee} miniFee={OPERATIONFEE} onChange={val => setFee(val)} />
+            </FeeContainer>
+            <WarningContainer>
+                <TezosIcon iconName="info" size={ms(5)} color="info" />
+                <InfoText>{warningTxt}</InfoText>
+            </WarningContainer>
+
+            <PasswordButtonContainer>
+                {!isLedger && (
+                    <PasswordInput
+                        label={t('general.nouns.wallet_password')}
+                        password={passPhrase}
+                        onChange={val => setPassPhrase(val)}
+                        containerStyle={{ width: '60%', marginTop: '10px' }}
+                    />
+                )}
+                <InvokeButton buttonTheme="primary" disabled={isDisabled} onClick={() => onInvokeOperation()}>
+                    {t('general.verbs.deposit')}
+                </InvokeButton>
+            </PasswordButtonContainer>
+            {isLedger && open && (
+                <WithdrawLedgerConfirmationModal
+                    amount={amount}
+                    fee={fee}
+                    source={selectedAccountHash}
+                    open={open}
+                    onClose={() => setOpen(false)}
+                    isLoading={isLoading}
+                />
+            )}
+        </Container>
     );
-
-    setOpen(false);
-    dispatch(setIsLoadingAction(false));
-
-    if (!!operationResult) {
-      onSuccess();
-    }
-  }
-
-  function onEnterPress(keyVal, disabled) {
-    if (keyVal === 'Enter' && !disabled) {
-      onInvokeOperation();
-    }
-  }
-
-  const { isIssue, warningMessage } = getBalanceState();
-  const isDisabled =
-    !isReady || isLoading || isIssue || amount === '0' || !amount || (!passPhrase && !isLedger);
-
-  const error = isIssue ? <InputError error={warningMessage} /> : '';
-
-  const warningTxt = t('components.withdrawDeposit.deposit_warning', {
-    managerAddress: selectedParentHash
-  });
-
-  return (
-    <Container onKeyDown={event => onEnterPress(event.key, isDisabled)}>
-      <AmountContainer>
-        <TezosNumericInput
-          decimalSeparator={t('general.decimal_separator')}
-          label={t('general.nouns.amount')}
-          amount={amount}
-          onChange={val => setAmount(val)}
-          errorText={error}
-        />
-        <UseMax onClick={onGetMax}>{t('general.verbs.use_max')}</UseMax>
-      </AmountContainer>
-      <FeeContainer>
-        <Fees
-          low={newFees.low}
-          medium={newFees.medium}
-          high={newFees.high}
-          fee={fee}
-          miniFee={OPERATIONFEE}
-          onChange={val => setFee(val)}
-        />
-      </FeeContainer>
-      <WarningContainer>
-        <TezosIcon iconName="info" size={ms(5)} color="info" />
-        <InfoText>{warningTxt}</InfoText>
-      </WarningContainer>
-
-      <PasswordButtonContainer>
-        {!isLedger && (
-          <PasswordInput
-            label={t('general.nouns.wallet_password')}
-            password={passPhrase}
-            onChange={val => setPassPhrase(val)}
-            containerStyle={{ width: '60%', marginTop: '10px' }}
-          />
-        )}
-        <InvokeButton
-          buttonTheme="primary"
-          disabled={isDisabled}
-          onClick={() => onInvokeOperation()}
-        >
-          {t('general.verbs.deposit')}
-        </InvokeButton>
-      </PasswordButtonContainer>
-      {isLedger && open && (
-        <WithdrawLedgerConfirmationModal
-          amount={amount}
-          fee={fee}
-          source={selectedAccountHash}
-          open={open}
-          onClose={() => setOpen(false)}
-          isLoading={isLoading}
-        />
-      )}
-    </Container>
-  );
 }
 
 export default Deposit;
