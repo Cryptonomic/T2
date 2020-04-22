caa23db75bc44835457d0b94f97ca02151e04359
anonymoussprocket <anonymoussprocket@gmail.com>
2020-04-22 17:58:21 -0400

    - transaction header rendering

--- src/contracts/TokenContract/components/Transactions/index.tsx
+++ src/contracts/TokenContract/components/Transactions/index.tsx
@@ -7,51 +7,38 @@ import Transaction from '../Transaction';
 import { TokenTransaction } from '../../../../types/general';
 
 const Container = styled.section`
-  height: 100%;
-  background-color: ${({ theme: { colors } }) => colors.white};
-  display: flex;
-  flex-direction: column;
-  flex: 1;
+    height: 100%;
+    background-color: ${({ theme: { colors } }) => colors.white};
+    display: flex;
+    flex-direction: column;
+    flex: 1;
 `;
 
 const SectionContainer = styled.div``;
 
 interface Props {
-  transactions: TokenTransaction[];
-  selectedParentHash: string;
-  symbol: string;
+    transactions: TokenTransaction[];
+    selectedParentHash: string;
+    symbol: string;
 }
 
 export default function Transactions(props: Props) {
-  const { transactions, selectedParentHash, symbol } = props;
+    const { transactions, selectedParentHash, symbol } = props;
 
-  const transactionsByDate = transactions.reduce((acc, curr) => {
-    const date = moment(curr.timestamp).format('l');
-    acc[date] = [...(acc[date] || []), curr];
-    return acc;
-  }, {});
+    const transactionsByDate = transactions.reduce((acc, curr) => {
+        const date = moment(curr.timestamp).format('LL');
+        acc[date] = [...(acc[date] || []), curr];
+        return acc;
+    }, {});
 
-  const renderDayTransactions = (day, grTransactions, grIndex) => (
-    <SectionContainer key={grIndex}>
-      <TransactionsLabel date={new Date(day)} />
-      {grTransactions.map((transaction, index) => {
-        return (
-          <Transaction
-            key={index}
-            transaction={transaction}
-            selectedParentHash={selectedParentHash}
-            symbol={symbol}
-          />
-        );
-      })}
-    </SectionContainer>
-  );
+    const renderDayTransactions = (day, grTransactions, grIndex) => (
+        <SectionContainer key={grIndex}>
+            <TransactionsLabel date={day} skipFormat={true} />
+            {grTransactions.map((transaction, index) => {
+                return <Transaction key={index} transaction={transaction} selectedParentHash={selectedParentHash} symbol={symbol} />;
+            })}
+        </SectionContainer>
+    );
 
-  return (
-    <Container>
-      {Object.keys(transactionsByDate).map((day, index) =>
-        renderDayTransactions(day, transactionsByDate[day], index)
-      )}
-    </Container>
-  );
+    return <Container>{Object.keys(transactionsByDate).map((day, index) => renderDayTransactions(day, transactionsByDate[day], index))}</Container>;
 }
