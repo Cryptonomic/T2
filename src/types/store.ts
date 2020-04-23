7d5e181d1bbea059991c376babed029c61fe2e4c
Darius <dariusz.sobkowicz@gmail.com>
2020-04-24 00:51:49 +0200

    modal types

--- src/types/store.ts
+++ src/types/store.ts
@@ -1,46 +1,51 @@
 import { Path, Node, NodeStatus, Identity, Token, AddressType } from './general';
 
 export interface AppState {
-  isLoading: boolean;
-  isWalletSyncing: boolean;
-  nodesStatus: NodeStatus;
-  time: Date;
-  isLedger: boolean;
-  isLedgerConnecting: boolean;
-  newVersion: string;
-  selectedAccountHash: string;
-  selectedParentHash: string;
-  selectedParentIndex: number;
-  selectedAccountIndex: number;
-  selectedAccountType: AddressType;
+    isLoading: boolean;
+    isWalletSyncing: boolean;
+    nodesStatus: NodeStatus;
+    time: Date;
+    isLedger: boolean;
+    isLedgerConnecting: boolean;
+    newVersion: string;
+    selectedAccountHash: string;
+    selectedParentHash: string;
+    selectedParentIndex: number;
+    selectedAccountIndex: number;
+    selectedAccountType: AddressType;
 }
 export interface WalletState {
-  identities: Identity[];
-  walletFileName: string;
-  walletLocation: string;
-  walletPassword: string;
-  tokens: Token[];
+    identities: Identity[];
+    walletFileName: string;
+    walletLocation: string;
+    walletPassword: string;
+    tokens: Token[];
 }
 
 export interface MessageState {
-  text: string;
-  isError: boolean;
-  hash: string;
-  localeParam: number;
+    text: string;
+    isError: boolean;
+    hash: string;
+    localeParam: number;
 }
 
 export interface SettingsState {
-  locale: string;
-  selectedNode: string;
-  nodesList: Node[];
-  selectedPath: string;
-  pathsList: Path[];
+    locale: string;
+    selectedNode: string;
+    nodesList: Node[];
+    selectedPath: string;
+    pathsList: Path[];
+}
+
+export interface ModalState {
+    open: boolean;
 }
 
 export interface RootState {
-  router: any;
-  wallet: WalletState;
-  message: MessageState;
-  settings: SettingsState;
-  app: AppState;
+    router: any;
+    wallet: WalletState;
+    message: MessageState;
+    settings: SettingsState;
+    app: AppState;
+    modal: ModalState;
 }
