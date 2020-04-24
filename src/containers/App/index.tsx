bb161810b26122346e54e0a5aa098db10754fd4b
Darius <dariusz.sobkowicz@gmail.com>
2020-04-24 02:27:16 +0200

    added listeners

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch, shallowEqual } from 'react-redux';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import styled from 'styled-components';
+import { ipcRenderer } from 'electron';
+
 import Home from '../Home';
 import Login from '../Login';
 import Settings from '../Settings';
@@ -10,7 +12,8 @@ import Loader from '../../components/Loader';
 import TopBar from '../../components/TopBar';
 import VersionStatus from '../../components/VersionStatus';
 import MessageBar from '../../components/MessageBar';
-
+import { createMessageAction } from '../../reduxContent/message/actions';
+import { setModalOpen, setModalDefaultMessage, setModalActiveTab } from '../../reduxContent/modal/actions';
 import { getNewVersionThunk } from '../../reduxContent/app/thunks';
 import { getIsNodesSelector } from '../../reduxContent/settings/selectors';
 import { getWalletName } from '../../reduxContent/wallet/selectors';
@@ -18,68 +21,84 @@ import { getLoggedIn } from '../../utils/login';
 import { RootState, WalletState, AppState } from '../../types/store';
 
 const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-  padding: 0;
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    padding: 0;
 `;
 
 function App() {
-  const dispatch = useDispatch();
-  const wallet = useSelector<RootState, WalletState>(state => state.wallet, shallowEqual);
-  const { newVersion, isLedger, isLoading } = useSelector<RootState, AppState>(
-    state => state.app,
-    shallowEqual
-  );
-  const walletName = useSelector(getWalletName);
-  const isNodes = useSelector(getIsNodesSelector);
-  const isLoggedIn = getLoggedIn(wallet);
+    const dispatch = useDispatch();
+    const wallet = useSelector<RootState, WalletState>(state => state.wallet, shallowEqual);
+    const { newVersion, isLedger, isLoading } = useSelector<RootState, AppState>(state => state.app, shallowEqual);
+    const walletName = useSelector(getWalletName);
+    const isNodes = useSelector(getIsNodesSelector);
+    const isLoggedIn = getLoggedIn(wallet);
 
-  useEffect(() => {
-    dispatch(getNewVersionThunk());
-  }, []);
+    useEffect(() => {
+        dispatch(getNewVersionThunk());
 
-  return (
-    <Container>
-      <TopBar walletName={walletName} isLoggedIn={isLoggedIn} isExtended={!!newVersion} />
-      {newVersion && <VersionStatus version={newVersion} />}
-      <Switch>
-        <Route
-          path="/home"
-          render={() => {
-            if (!isNodes) {
-              return <Redirect to="/walletNodesRequired" />;
+        ipcRenderer.on('login', (event, msg) => {
+            dispatch(createMessageAction(msg, true));
+        });
+
+        ipcRenderer.on('wallet', (event, url) => {
+            const sampleUrl = { type: 'plain', text: 'text' };
+            dispatch(setModalDefaultMessage(sampleUrl.text));
+
+            if (sampleUrl.type === 'plain') {
+                dispatch(setModalActiveTab(0));
             }
 
-            if (!isLoggedIn && !isLedger) {
-              return <Redirect to="/login" />;
+            if (sampleUrl.type === 'auth') {
+                dispatch(setModalActiveTab(1));
             }
 
-            return <Home />;
-          }}
-        />
-        <Route path="/walletNodesRequired" component={WalletNodesRequired} />
-        <Route path="/settings" component={Settings} />
-        <Route
-          path="/login"
-          render={routerProps => {
-            if (!isNodes) {
-              return <Redirect to="/walletNodesRequired" />;
-            }
+            dispatch(setModalOpen(true));
+        });
+    }, []);
 
-            if (isLoggedIn) {
-              return <Redirect to="/home" />;
-            }
+    return (
+        <Container>
+            <TopBar walletName={walletName} isLoggedIn={isLoggedIn} isExtended={!!newVersion} />
+            {newVersion && <VersionStatus version={newVersion} />}
+            <Switch>
+                <Route
+                    path="/home"
+                    render={() => {
+                        if (!isNodes) {
+                            return <Redirect to="/walletNodesRequired" />;
+                        }
 
-            return <Login {...routerProps} />;
-          }}
-        />
-        <Redirect from="/" to="/home" />
-      </Switch>
-      <MessageBar />
-      {isLoading && <Loader />}
-    </Container>
-  );
+                        if (!isLoggedIn && !isLedger) {
+                            return <Redirect to="/login" />;
+                        }
+
+                        return <Home />;
+                    }}
+                />
+                <Route path="/walletNodesRequired" component={WalletNodesRequired} />
+                <Route path="/settings" component={Settings} />
+                <Route
+                    path="/login"
+                    render={routerProps => {
+                        if (!isNodes) {
+                            return <Redirect to="/walletNodesRequired" />;
+                        }
+
+                        if (isLoggedIn) {
+                            return <Redirect to="/home" />;
+                        }
+
+                        return <Login {...routerProps} />;
+                    }}
+                />
+                <Redirect from="/" to="/home" />
+            </Switch>
+            <MessageBar />
+            {isLoading && <Loader />}
+        </Container>
+    );
 }
 
 export default App;
