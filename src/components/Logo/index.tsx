e69368c767d2e2f79e438426fc62cf9eea49ebbb
anonymoussprocket <anonymoussprocket@gmail.com>
2020-04-28 20:50:16 -0400

    - logo opacity

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -10,30 +10,29 @@ const tezosLogo = require(`../../../resources/tezosLogo.svg`); // eslint-disable
 const customLogo = require(`../../../resources/${logo}`); // eslint-disable-line import/no-dynamic-require
 
 const Tz = styled.div`
-  width: ${ms(7)};
-  height: ${ms(7)};
-  background-color: ${({ theme: { colors } }) => colors.accent};
-  mask: url(${tezosLogo.default}) no-repeat;
-  mask-size: cover;
+    width: ${ms(7)};
+    height: ${ms(7)};
+    background-color: ${({ theme: { colors } }) => colors.accent};
+    mask: url(${tezosLogo.default}) no-repeat;
+    mask-size: cover;
 `;
 
 const LogoImg = styled.img`
-  max-height: ${ms(7)};
+    max-height: ${ms(7)};
 `;
 
 const ButtonContainer = styled(Button)`
-  text-align: center;
-  min-width: 57px;
-  margin: 0 5px;
-  color: ${({ theme: { colors } }) => colors.primary};
-  opacity: 0.6;
+    text-align: center;
+    min-width: 57px;
+    margin: 0 5px;
+    color: ${({ theme: { colors } }) => colors.primary};
 `;
 
 const Logo = () => (
-  <ButtonContainer onClick={() => openLink(logoLink)} buttonTheme="plain">
-    {logo.length === 0 && <Tz />}
-    {logo.length > 0 && <LogoImg src={customLogo.default} />}
-  </ButtonContainer>
+    <ButtonContainer onClick={() => openLink(logoLink)} buttonTheme="plain">
+        {logo.length === 0 && <Tz />}
+        {logo.length > 0 && <LogoImg src={customLogo.default} />}
+    </ButtonContainer>
 );
 
 export default Logo;
