bcfc2634e3c06f73496600d802dcfb7cb78b835f
anonymoussprocket <anonymoussprocket@gmail.com>
2020-05-01 15:35:23 -0400

    - StakerDAO info improvements

--- src/contracts/StakerToken/components/Details/style.ts
+++ src/contracts/StakerToken/components/Details/style.ts
@@ -20,3 +20,12 @@ export const LinkTitle = styled.span`
     font-size: 12px;
     text-decoration: underline;
 `;
+
+export const ContentTitle = styled.div`
+    font-size: 24px;
+    font-weight: 300;
+    line-height: 34px;
+    color: ${({ theme: { colors } }) => colors.primary};
+    letter-spacing: 1px;
+    margin-bottom: 32px;
+`;
