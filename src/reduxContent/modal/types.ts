1117238c9e65a68c77f0760c14ac725407bcd319
anonymoussprocket <anonymoussprocket@gmail.com>
2020-05-05 02:15:42 -0400

    - schemes manual merge

---
+++ src/reduxContent/modal/types.ts
@@ -0,0 +1,4 @@
+export const SET_MODAL_OPEN = 'SET_MODAL_OPEN';
+export const SET_MODAL_ACTIVE_TAB = 'SET_MODAL_ACTIVE_TAB';
+export const SET_MODAL_VALUE = 'SET_MODAL_VALUE';
+export const CLEAR_MODAL = 'CLEAR_MODAL';
