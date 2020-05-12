80c3adaf5c0ab1abfb3a719d86cc5f8a4596e09f
Darius <dariusz.sobkowicz@gmail.com>
2020-05-12 13:33:41 +0200

    refactor login condition

--- src/containers/LoginConditions/index.tsx
+++ src/containers/LoginConditions/index.tsx
@@ -1,41 +1,16 @@
 import React from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import { goBack as back } from 'react-router-redux';
-import styled from 'styled-components';
-import BackCaret from '@material-ui/icons/KeyboardArrowLeft';
+import { useParams } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
-import { ms } from '../../styles/helpers';
+import BackButton from '../../components/BackButton';
 import { H2 } from '../../components/Heading';
 import PdfReader from '../../components/PdfReader';
 import { termsService, privacyPolicy } from '../../config.json';
+import { Container, BackButtonContainer } from './style';
 
-interface Props {
-    match?: any;
-    goBack: () => {};
-}
-
-const BackToWallet = styled.div`
-    display: flex;
-    align-items: center;
-    align-self: flex-start;
-    color: #4486f0;
-    cursor: pointer;
-    margin-bottom: 1rem;
-`;
-
-const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    padding: ${ms(3)} ${ms(10)};
-    align-items: center;
-    flex-grow: 1;
-`;
-
-function LoginConditions(props: Props) {
-    const { match, goBack } = props;
-    const { type } = match.params;
-
+const LoginConditions = () => {
+    const { type } = useParams();
+    const { t } = useTranslation();
     let title = 'Privacy Policy';
     let url = privacyPolicy;
     if (type === 'termsOfService') {
@@ -45,28 +20,13 @@ function LoginConditions(props: Props) {
 
     return (
         <Container>
-            <BackToWallet onClick={goBack}>
-                <BackCaret
-                    style={{
-                        fill: '#4486f0',
-                        height: '28px',
-                        width: '28px',
-                        marginRight: '5px',
-                        marginLeft: '-9px',
-                        marginTop: '4px'
-                    }}
-                />
-                <span>Back</span>
-            </BackToWallet>
-
+            <BackButtonContainer>
+                <BackButton label={t('general.back')} />
+            </BackButtonContainer>
             <H2>{title}</H2>
             <PdfReader pdfUrl={url} />
         </Container>
     );
-}
+};
 
-function mapDispatchToProps(d) {
-    return bindActionCreators({ goBack: () => dd => dd(back()) }, d);
-}
-
-export default connect(null, mapDispatchToProps)(LoginConditions);
+export default LoginConditions;
