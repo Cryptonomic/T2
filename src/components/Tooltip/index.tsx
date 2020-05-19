b4008c1f09f00aa35b67188d9895abe7638c6803
Darius <dariusz.sobkowicz@gmail.com>
2020-05-19 12:03:56 +0200

    use material tooltip

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -1,247 +1,49 @@
 import React from 'react';
-import RCTooltip from 'rc-tooltip';
-
+import Tooltip from '@material-ui/core/Tooltip';
 import styled from 'styled-components';
-import { stripUnit, lighten } from 'polished';
+import { lighten } from 'polished';
+
 import { ms } from '../../styles/helpers';
+import { TooltipProps } from './types';
 
-interface AdapterProps {
-    className: string;
-    offset: [];
-    overlay: string | React.ReactNode;
-}
-
-const TooltipAdapter = ({ className, offset, ...props }: AdapterProps) => {
-    return <RCTooltip {...props} overlayClassName={`${className}__overlay`} prefixCls={`${className}__tooltip`} align={{ offset }} />;
-};
-
-const arrowWidth = '10px';
-const tooltipDistance = '14px';
-const tooltipDistanceUnitless = stripUnit(tooltipDistance);
-const arrowWidthUnitless = stripUnit(arrowWidth);
-
-interface TooltipProps {
-    arrowPos: any;
-    className: any;
-    offset: any;
-    placement: any;
-    overlay: any;
-    boxShadow: any;
-}
-
-const StyledTooltip = styled(TooltipAdapter)<
-    Pick<TooltipProps, 'arrowPos' | 'className' | 'offset' | 'placement' | 'overlay' | 'boxShadow'> & React.HTMLAttributes<any>
->`
-    cursor: pointer;
-    -webkit-app-region: no-drag;
-    display: flex !important;
-    align-items: center;
-
-    &__tooltip {
-        position: absolute;
-        z-index: 9999;
-        display: block;
-        visibility: visible;
-        font-size: ${ms(1)};
-        opacity: 1;
-
-        &-hidden {
-            display: none;
-        }
-
-        &-placement-top,
-        &-placement-topLeft,
-        &-placement-topRight {
-            padding: ${arrowWidth} 0 ${tooltipDistance} 0;
-        }
-        &-placement-right,
-        &-placement-rightTop,
-        &-placement-rightBottom {
-            padding: 0 ${arrowWidth} 0 ${tooltipDistance};
-        }
-        &-placement-bottom,
-        &-placement-bottomLeft,
-        &-placement-bottomRight {
-            padding: ${tooltipDistance} 0 ${arrowWidthUnitless / 2}px 0;
-        }
-        &-placement-left,
-        &-placement-leftTop,
-        &-placement-leftBottom {
-            padding: 0 ${tooltipDistance} 0 ${arrowWidth};
-        }
-
-        &-inner {
-            padding: ${ms(-2)};
-            color: ${({ theme: { colors } }) => colors.primary};
-            text-align: left;
-            text-decoration: none;
-            background-color: ${({ theme: { colors } }) => colors.white};
-            border: 1px solid ${({ theme: { colors } }) => lighten(0.1, colors.secondary)};
-            box-shadow: ${({ boxShadow, theme: { colors } }) => (boxShadow ? `0 0 4px 0 ${colors.index0}` : '')};
-            -webkit-app-region: no-drag;
-        }
-
-        &-arrow {
-            width: 0;
-            height: 0;
-            position: absolute;
-
-            &::after {
+const StyledTooltip = styled(props => <Tooltip classes={{ popper: props.className, tooltip: 'tooltip' }} {...props} />)`
+    & .tooltip {
+        padding: ${ms(-2)};
+        color: ${({ theme: { colors } }) => colors.primary};
+        text-align: left;
+        text-decoration: none;
+        background-color: ${({ theme: { colors } }) => colors.white};
+        border: 1px solid ${({ theme: { colors } }) => lighten(0.1, colors.secondary)};
+        box-shadow: ${({ theme: { colors } }) => `0 0 4px 0 ${colors.index0}`};
+        & .MuiTooltip-arrow {
+            color: ${({ theme: { colors } }) => lighten(0.1, colors.secondary)};
+            bottom: ${({ placement }) => (placement === 'top' ? '-1px !important' : '')};
+            right: ${({ placement }) => (placement === 'left' ? '-1px !important' : '')};
+            &:after {
+                border: solid transparent;
                 content: '';
-                display: block;
+                height: 0;
+                width: 0;
                 position: absolute;
-                z-index: -1;
+                pointer-events: none;
+                border-top-color: ${({ placement, theme: { colors } }) => (placement === 'top' ? colors.white : '')};
+                border-right-color: ${({ placement, theme: { colors } }) => (placement === 'right' ? colors.white : '')};
+                border-bottom-color: ${({ placement, theme: { colors } }) => (placement === 'bottom' ? colors.white : '')};
+                border-left-color: ${({ placement, theme: { colors } }) => (placement === 'left' ? colors.white : '')};
+                border-width: 1em;
+                top: ${({ placement }) => (placement === 'bottom' ? '-0.8333em' : placement === 'left' || placement === 'right' ? '0' : '')};
+                right: ${({ placement }) => (placement === 'left' ? '-0.8333em' : '')};
+                bottom: ${({ placement }) => (placement === 'top' ? '-0.8333em' : '')};
+                left: ${({ placement }) => (placement === 'right' ? '-0.8333em' : '')};
             }
-        }
-
-        &-placement-top &-arrow,
-        &-placement-topLeft &-arrow,
-        &-placement-topRight &-arrow {
-            bottom: ${arrowWidthUnitless - 5}px;
-            margin-left: -${arrowWidth};
-            border-left: ${arrowWidthUnitless + 7}px solid transparent;
-            border-right: ${arrowWidthUnitless + 7}px solid transparent;
-            border-top: ${arrowWidth} solid ${({ theme: { colors } }) => colors.white};
-
-            &::after {
-                border-left: ${arrowWidthUnitless + 7}px solid transparent;
-                border-right: ${arrowWidthUnitless + 7}px solid transparent;
-                border-top: ${arrowWidth} solid ${({ theme: { colors } }) => colors.secondary};
-                bottom: -1px;
-                right: -${arrowWidthUnitless + 7}px;
-                margin-left: -${arrowWidth};
-            }
-        }
-
-        &-placement-top &-arrow {
-            left: ${props => (props.arrowPos && props.arrowPos.left ? props.arrowPos.left : '50%')};
-        }
-
-        &-placement-topLeft &-arrow {
-            left: 15%;
-        }
-
-        &-placement-topRight &-arrow {
-            right: 15%;
-        }
-
-        &-placement-right &-arrow,
-        &-placement-rightTop &-arrow,
-        &-placement-rightBottom &-arrow {
-            left: ${tooltipDistanceUnitless - arrowWidthUnitless + 1}px;
-            margin-top: -${arrowWidthUnitless + 7}px;
-            border-top: ${arrowWidthUnitless + 7}px solid transparent;
-            border-bottom: ${arrowWidthUnitless + 7}px solid transparent;
-            border-right: ${arrowWidth} solid ${({ theme: { colors } }) => colors.white};
-
-            &::after {
-                border-top: ${arrowWidthUnitless + 7}px solid transparent;
-                border-bottom: ${arrowWidthUnitless + 7}px solid transparent;
-                border-right: ${arrowWidth} solid ${({ theme: { colors } }) => colors.secondary};
-                left: -1px;
-                bottom: -${arrowWidthUnitless + 7}px;
-            }
-        }
-
-        &-placement-right &-arrow {
-            top: 50%;
-        }
-
-        &-placement-rightTop &-arrow {
-            top: 15%;
-            margin-top: 0;
-        }
-
-        &-placement-rightBottom &-arrow {
-            bottom: 15%;
-        }
-
-        &-placement-left &-arrow,
-        &-placement-leftTop &-arrow,
-        &-placement-leftBottom &-arrow {
-            right: ${tooltipDistanceUnitless - arrowWidthUnitless + 1}px;
-            margin-top: -${arrowWidthUnitless + 7}px;
-            border-top: ${arrowWidthUnitless + 7}px solid transparent;
-            border-bottom: ${arrowWidthUnitless + 7}px solid transparent;
-            border-left: ${arrowWidthUnitless}px solid ${({ theme: { colors } }) => colors.white};
-
-            &::after {
-                border-top: ${arrowWidthUnitless + 7}px solid transparent;
-                border-bottom: ${arrowWidthUnitless + 7}px solid transparent;
-                border-left: ${arrowWidthUnitless}px solid ${({ theme: { colors } }) => colors.secondary};
-                right: -1px;
-                bottom: -${arrowWidthUnitless + 7}px;
-            }
-        }
-
-        &-placement-left &-arrow {
-            top: 50%;
-        }
-
-        &-placement-leftTop &-arrow {
-            top: 15%;
-            margin-top: 0;
-        }
-
-        &-placement-leftBottom &-arrow {
-            bottom: 15%;
-        }
-
-        &-placement-bottom &-arrow,
-        &-placement-bottomLeft &-arrow,
-        &-placement-bottomRight &-arrow {
-            top: ${tooltipDistanceUnitless - arrowWidthUnitless + 1}px;
-            margin-left: -${arrowWidth};
-            border-left: ${arrowWidthUnitless + 7}px solid transparent;
-            border-right: ${arrowWidthUnitless + 7}px solid transparent;
-            border-bottom: ${arrowWidth} solid ${({ theme: { colors } }) => colors.white};
-
-            &::after {
-                border-left: ${arrowWidthUnitless + 7}px solid transparent;
-                border-right: ${arrowWidthUnitless + 7}px solid transparent;
-                border-bottom: ${arrowWidth} solid ${({ theme: { colors } }) => colors.secondary};
-                right: -${arrowWidthUnitless + 7}px;
-                bottom: -${arrowWidthUnitless - 1}px;
-            }
-        }
-
-        &-placement-bottom &-arrow {
-            left: ${props => (props.arrowPos && props.arrowPos.left ? props.arrowPos.left : '70%')};
-        }
-
-        &-placement-bottomLeft &-arrow {
-            left: 2%;
-        }
-
-        &-placement-bottomRight &-arrow {
-            right: 2%;
         }
     }
 `;
 
-interface Props {
-    content: string | React.ReactNode;
-    trigger?: string[];
-    children?: React.ReactNode;
-    className?: string;
-    position?: string;
-    arrowPos?: object;
-    offset?: any[];
-    boxShadow?: boolean;
-}
+const TooltipWrapper = ({ children, content, position }: TooltipProps) => (
+    <StyledTooltip arrow={true} interactive={true} title={content} placement={position}>
+        {children}
+    </StyledTooltip>
+);
 
-function Tooltip(props: Props) {
-    const { children, className, content, position, arrowPos, offset, boxShadow, ...restOfProps } = props;
-    return (
-        <StyledTooltip placement={position} className={className} overlay={content} arrowPos={arrowPos} offset={offset} boxShadow={boxShadow} {...restOfProps}>
-            {children}
-        </StyledTooltip>
-    );
-}
-
-Tooltip.defaultProps = {
-    trigger: ['hover'],
-    offset: ['-22%', 0]
-};
-
-export default Tooltip;
+export default TooltipWrapper;
