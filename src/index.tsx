2c36de9ec14c43bf84424e412c477c0b7341fe64
anonymoussprocket <anonymoussprocket@gmail.com>
2020-06-12 13:05:55 -0400

    - PR feedback: loglevel in config

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { registerFetch, registerLogger } from 'conseiljs';
 import fetch from 'node-fetch';
 import * as log from 'loglevel';
 
+import { logLevel } from './config.json';
 import Root from './containers/Root';
 import { configureStore, history } from './store/configureStore';
 import './utils/i18n';
@@ -13,7 +14,7 @@ import './styles/app.global.scss';
 const store = configureStore();
 
 const logger = log.getLogger('conseiljs');
-logger.setLevel('debug', false);
+logger.setLevel(logLevel as log.LogLevelDesc, false);
 registerLogger(logger);
 registerFetch(fetch);
 
