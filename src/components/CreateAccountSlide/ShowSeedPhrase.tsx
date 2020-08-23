21f5670332cb52af18113fc651fba3850e21572b
Karolina Szewczyk <szewczyk.karolinka@hotmail.com>
2020-08-23 22:37:54 +0200

    Added id for phase in Create Wallet

--- src/components/CreateAccountSlide/ShowSeedPhrase.tsx
+++ src/components/CreateAccountSlide/ShowSeedPhrase.tsx
@@ -2,88 +2,69 @@ import React, { Fragment } from 'react';
 import { useTranslation } from 'react-i18next';
 import CopyButton from '../CopyButton';
 
-import {
-  DescriptionContainer,
-  IconContainer,
-  RefreshButton,
-  RefreshIconWrapper,
-  SeedsContainer,
-  SeedColume,
-  SeedItem,
-  SeedIndex
-} from './style';
+import { DescriptionContainer, IconContainer, RefreshButton, RefreshIconWrapper, SeedsContainer, SeedColume, SeedItem, SeedIndex } from './style';
 
 interface Props {
-  seed: string;
-  onUpdate: () => void;
+    id?: string;
+    seed: string;
+    onUpdate: () => void;
 }
 
 function ShowSeedPhrase(props: Props) {
-  const { seed, onUpdate } = props;
-  const { t } = useTranslation();
+    const { id, seed, onUpdate } = props;
+    const { t } = useTranslation();
 
-  function setupSeedColumns() {
-    const seedWords = seed.split(' ');
-    const seedsTable: any[] = [];
-    let seedColums: string[] = [];
+    function setupSeedColumns() {
+        const seedWords = seed.split(' ');
+        const seedsTable: any[] = [];
+        let seedColums: string[] = [];
 
-    seedWords.forEach((item, index) => {
-      seedColums.push(item);
-      if ((index + 1) % 6 === 0) {
-        seedsTable.push(seedColums);
-        seedColums = [];
-      }
-    });
+        seedWords.forEach((item, index) => {
+            seedColums.push(item);
+            if ((index + 1) % 6 === 0) {
+                seedsTable.push(seedColums);
+                seedColums = [];
+            }
+        });
 
-    return seedsTable;
-  }
-  const seeds = setupSeedColumns();
-  const processedSeed = seed
-    .split(' ')
-    .map((item, index) => {
-      return `${index + 1}. ${item}`;
-    })
-    .join(' ');
-  return (
-    <Fragment>
-      <DescriptionContainer>
-        {t('components.createAccountSlide.descriptions.description1')}
-      </DescriptionContainer>
-      <SeedsContainer>
-        {seeds.map((items, index) => {
-          return (
-            <SeedColume key={index}>
-              {items.map((item, index1) => (
-                <SeedItem key={index1}>
-                  <SeedIndex>{index * 6 + index1 + 1}</SeedIndex>
-                  {item}
-                </SeedItem>
-              ))}
-            </SeedColume>
-          );
-        })}
-      </SeedsContainer>
-      <IconContainer>
-        <div>
-          <CopyButton
-            title={t('components.createAccountSlide.copy_seed')}
-            text={processedSeed}
-            color="#2c7df7"
-          />
-        </div>
-        <div>
-          <RefreshButton
-            color="secondary"
-            disableRipple={true}
-            onClick={() => onUpdate()}
-            startIcon={<RefreshIconWrapper />}
-          >
-            {t('components.createAccountSlide.generate_other_seed')}
-          </RefreshButton>
-        </div>
-      </IconContainer>
-    </Fragment>
-  );
+        return seedsTable;
+    }
+    const seeds = setupSeedColumns();
+    const processedSeed = seed
+        .split(' ')
+        .map((item, index) => {
+            return `${index + 1}. ${item}`;
+        })
+        .join(' ');
+    return (
+        <Fragment>
+            <DescriptionContainer>{t('components.createAccountSlide.descriptions.description1')}</DescriptionContainer>
+            <SeedsContainer id={id}>
+                {seeds.map((items, index) => {
+                    return (
+                        <SeedColume id="test-phase-column" key={index}>
+                            {items.map((item, index1) => (
+                                <SeedItem id="test-item" key={index1}>
+                                    <SeedIndex>{index * 6 + index1 + 1}</SeedIndex>
+                                    {item}
+                                </SeedItem>
+                            ))}
+                        </SeedColume>
+                    );
+                })}
+            </SeedsContainer>
+            <IconContainer>
+                <div>
+                    <CopyButton title={t('components.createAccountSlide.copy_seed')} text={processedSeed} color="#2c7df7" />
+                </div>
+                <div>
+                    <RefreshButton color="secondary" disableRipple={true} onClick={() => onUpdate()} startIcon={<RefreshIconWrapper />}>
+                        {t('components.createAccountSlide.generate_other_seed')}
+                    </RefreshButton>
+                </div>
+            </IconContainer>
+        </Fragment>
+    );
 }
 
 export default ShowSeedPhrase;
--- src/components/CreateAccountSlide/index.tsx
+++ src/components/CreateAccountSlide/index.tsx
@@ -10,93 +10,77 @@ import { generateNewMnemonic } from '../../utils/general';
 import useEventListener from '../../customHooks/useEventListener';
 import { importAddressThunk } from '../../reduxContent/wallet/thunks';
 
-import {
-  CreateAccountSlideContainer,
-  BackButtonContainer,
-  ActionButton,
-  DescriptionContainer,
-  TitleContainer
-} from './style';
+import { CreateAccountSlideContainer, BackButtonContainer, ActionButton, DescriptionContainer, TitleContainer } from './style';
 
 function CreateAccountSlide() {
-  const { t } = useTranslation();
-  const dispatch = useDispatch();
-  const [isDisabled, setIsDisabled] = useState(false);
-  const [seed, setSeed] = useState(generateNewMnemonic());
-  const [currentSlide, setCurrentSlide] = useState(0);
+    const { t } = useTranslation();
+    const dispatch = useDispatch();
+    const [isDisabled, setIsDisabled] = useState(false);
+    const [seed, setSeed] = useState(generateNewMnemonic());
+    const [currentSlide, setCurrentSlide] = useState(0);
 
-  function updateMnemonic() {
-    setSeed(generateNewMnemonic());
-  }
-
-  function onImport() {
-    dispatch(importAddressThunk(GENERATE_MNEMONIC, seed));
-  }
-
-  function onAction() {
-    if (currentSlide === 0) {
-      setIsDisabled(true);
-      setCurrentSlide(currentSlide + 1);
-    } else if (currentSlide === 2) {
-      onImport();
-    } else {
-      setCurrentSlide(currentSlide + 1);
+    function updateMnemonic() {
+        setSeed(generateNewMnemonic());
     }
-  }
 
-  const onKeyPressed = event => {
-    if (event.key === 'Enter' && (currentSlide !== 1 || !isDisabled)) {
-      onAction();
+    function onImport() {
+        dispatch(importAddressThunk(GENERATE_MNEMONIC, seed));
     }
-  };
 
-  const createAccount = () => {
+    function onAction() {
+        if (currentSlide === 0) {
+            setIsDisabled(true);
+            setCurrentSlide(currentSlide + 1);
+        } else if (currentSlide === 2) {
+            onImport();
+        } else {
+            setCurrentSlide(currentSlide + 1);
+        }
+    }
+
+    const onKeyPressed = (event) => {
+        if (event.key === 'Enter' && (currentSlide !== 1 || !isDisabled)) {
+            onAction();
+        }
+    };
+
+    const createAccount = () => {
+        return (
+            <Fragment>
+                <TitleContainer>{t('components.createAccountSlide.seed_backup')}</TitleContainer>
+                <DescriptionContainer>{t('components.createAccountSlide.descriptions.description2')}</DescriptionContainer>
+            </Fragment>
+        );
+    };
+
+    function mainRender() {
+        switch (currentSlide) {
+            case 0:
+                return <ShowSeedPhrase id="test-create-account-list-seed" seed={seed} onUpdate={() => updateMnemonic()} />;
+            case 1:
+                return <BackUpSeedPhrase seed={seed} onValid={(isVal) => setIsDisabled(isVal)} />;
+            default:
+                return createAccount();
+        }
+    }
+
+    useEventListener('keydown', onKeyPressed);
+
+    const buttonTitle = currentSlide === 2 ? t('components.createAccountSlide.create_account') : t('general.next');
+
     return (
-      <Fragment>
-        <TitleContainer>{t('components.createAccountSlide.seed_backup')}</TitleContainer>
-        <DescriptionContainer>
-          {t('components.createAccountSlide.descriptions.description2')}
-        </DescriptionContainer>
-      </Fragment>
+        <CreateAccountSlideContainer>
+            {!!currentSlide && (
+                <BackButtonContainer>
+                    <BackButton label={t('components.createAccountSlide.back_to_seed')} onClick={() => setCurrentSlide(0)} />
+                </BackButtonContainer>
+            )}
+            {mainRender()}
+            <ActionButton buttonTheme="primary" disabled={currentSlide === 1 && isDisabled} onClick={() => onAction()}>
+                {buttonTitle}
+            </ActionButton>
+        </CreateAccountSlideContainer>
     );
-  };
-
-  function mainRender() {
-    switch (currentSlide) {
-      case 0:
-        return <ShowSeedPhrase seed={seed} onUpdate={() => updateMnemonic()} />;
-      case 1:
-        return <BackUpSeedPhrase seed={seed} onValid={isVal => setIsDisabled(isVal)} />;
-      default:
-        return createAccount();
-    }
-  }
-
-  useEventListener('keydown', onKeyPressed);
-
-  const buttonTitle =
-    currentSlide === 2 ? t('components.createAccountSlide.create_account') : t('general.next');
-
-  return (
-    <CreateAccountSlideContainer>
-      {!!currentSlide && (
-        <BackButtonContainer>
-          <BackButton
-            label={t('components.createAccountSlide.back_to_seed')}
-            onClick={() => setCurrentSlide(0)}
-          />
-        </BackButtonContainer>
-      )}
-      {mainRender()}
-      <ActionButton
-        buttonTheme="primary"
-        disabled={currentSlide === 1 && isDisabled}
-        onClick={() => onAction()}
-      >
-        {buttonTitle}
-      </ActionButton>
-    </CreateAccountSlideContainer>
-  );
 }
 
 export default CreateAccountSlide;
