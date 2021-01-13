2e633572c035e02e592509d3ef491dcc074fd12c
anonymoussprocket <anonymoussprocket@gmail.com>
2021-01-13 15:37:30 -0500

    - missing style

--- src/components/BalanceBanner/style.ts
+++ src/components/BalanceBanner/style.ts
@@ -51,6 +51,12 @@ export const Gap = styled.div`
     width: 10px;
 `;
 
+export const Delegate = styled.span`
+    font-weight: 500;
+    margin-left: 3px;
+    margin-right: 3px;
+`;
+
 export const DelegateName = styled.span`
     font-weight: 500;
     margin-left: 3px;
