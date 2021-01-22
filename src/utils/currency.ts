d168c57e98ad56c636371d4c9d68425e303428c4
anonymoussprocket <anonymoussprocket@gmail.com>
2021-01-22 12:36:29 -0500

    - number format

--- src/utils/currency.ts
+++ src/utils/currency.ts
@@ -1,15 +1,26 @@
 import { BigNumber } from 'bignumber.js';
-const utez = 1000000;
+const utez = 1_000_000;
 
 export function utezToTez(amount: number): number {
-    return amount / utez;
+    const b = new BigNumber(amount);
+    const t = b.dividedBy(new BigNumber(utez));
+
+    return t.toNumber();
 }
 
 export function tezToUtez(amount: number | string): number {
-    const x = new BigNumber(amount);
-    return x.multipliedBy(utez).toNumber();
+    const b = new BigNumber(amount);
+    const m = b.multipliedBy(utez);
+
+    return m.toNumber();
 }
 
+/**
+ * Assumes the input amount to be in µtz. The amount is multiplied by 1_000_000 and then formatted to the requested number of decimal places.
+ */
 export function formatAmount(amount, decimal: number = 6) {
-    return utezToTez(amount).toFixed(decimal);
+    const b = new BigNumber(amount);
+    const m = b.dividedBy(utez);
+
+    return m.toFormat(decimal);
 }
