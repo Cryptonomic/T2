6109c4ef5ea1803fcd340c9f5d0bbebfcace120f
anonymoussprocket <anonymoussprocket@gmail.com>
2021-01-22 12:27:39 -0500

    - contract metadata

--- src/constants/Token.ts
+++ src/constants/Token.ts
@@ -185,3 +185,48 @@ export const knownTokenContracts: (Token | VaultToken)[] = [
         round: 6,
     },
 ];
+
+export const knownContractNames = {
+    KT1EctCuorV2NfVb1XTQgvzJ88MQtWP8cMMv: 'StakerDAO Token',
+    KT1PWx2mnDueood7fEmfbBDKx1D9BAnnXitn: 'tzBTC Token',
+    KT1LN4LPSqTMS7Sd2CJw4bbDGRkMv2t68Fy9: 'USDtz Token',
+    KT19at7rQUvyjxnZ2fBv7D9zc8rkyG7gAoU8: 'ETHtz Token',
+    KT1VYsVfmobT7rsMVivvZ4J8i3bPiqz12NaH: 'wXTZ Token',
+    KT1V4Vp7zhynCNuaBjWMpNU535Xm2sgqkz6M: 'wXTZ Manager',
+    KT19c8n5mWrqpxMcR3J687yssHxotj88nGhZ: 'Dexter ETHtz/XTZ Pool',
+    KT1Puc9St8wdNoGtLiD2WXaHbWU7styaxYhD: 'Dexter USDtz/XTZ Pool',
+    KT1DrJV8vhkdLEj76h1H9Q4irZDqAkMPo1Qf: 'Dexter tzBTC/XTZ Pool',
+    KT1XTXBsEauzcv3uPvVXW92mVqrx99UGsb9T: 'Dexter wXTZ/XTZ Pool',
+    KT1H28iie4mW9LmmJeYLjH6zkC8wwSmfHf5P: 'TzButton Round 2',
+};
+
+export const knownMarketMetadata = [
+    {
+        network: 'mainnet',
+        address: 'KT19c8n5mWrqpxMcR3J687yssHxotj88nGhZ',
+        name: 'Dexter ETHtz/XTZ Pool',
+        scale: 18,
+        symbol: 'ETHtz',
+    },
+    {
+        network: 'mainnet',
+        address: 'KT1Puc9St8wdNoGtLiD2WXaHbWU7styaxYhD',
+        name: 'Dexter USDtz/XTZ Pool',
+        scale: 6,
+        symbol: 'USDtz',
+    },
+    {
+        network: 'mainnet',
+        address: 'KT1DrJV8vhkdLEj76h1H9Q4irZDqAkMPo1Qf',
+        name: 'Dexter tzBTC/XTZ Pool',
+        scale: 8,
+        symbol: 'tzBTC',
+    },
+    {
+        network: 'mainnet',
+        address: 'KT1XTXBsEauzcv3uPvVXW92mVqrx99UGsb9T',
+        name: 'Dexter wXTZ/XTZ Pool',
+        scale: 6,
+        symbol: 'wXTZ',
+    },
+];
