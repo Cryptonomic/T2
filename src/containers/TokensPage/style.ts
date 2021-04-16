3faa64d528f31ede24ebe3ecca8047a3f6b865f9
Darius <dariusz.sobkowicz@gmail.com>
2021-04-16 10:19:05 +0200

    pointer link

--- src/containers/TokensPage/style.ts
+++ src/containers/TokensPage/style.ts
@@ -69,6 +69,7 @@ export const BoxDescription = styled.div`
 
 export const BlueLink = styled.span<{ isActive: boolean | undefined }>`
     color: ${({ isActive }) => (isActive ? '#2F80ED' : '')};
+    cursor: pointer;
 `;
 
 export const TokensTitle = styled.div`
