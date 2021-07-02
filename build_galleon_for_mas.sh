c7dbe85091bce07e5b9a7eaee4e2412eead6f6dc
Darius <dariusz.sobkowicz@gmail.com>
2021-07-02 11:16:26 +0200

    update mas build

--- build_galleon_for_mas.sh
+++ build_galleon_for_mas.sh
@@ -8,11 +8,14 @@ cd ./src
 npm install
 cd ..
 
+#~~~ Copy main.dev.js
+cp ../Internal-infrastructure/deployments/prod/Galleon/assets/mas/main.dev.js ./src
+
 #~~~ Build
 npm run package
 
 #~~~ Copies build fixes
-cp ../Internal-infrastructure/deployments/prod/Galleon/assets/mas/build-mas.sh ./release/mas
+cp ../Internal-infrastructure/deployments/prod/Galleon/build-mas.sh ./release/mas
 cp ../Internal-infrastructure/deployments/prod/Galleon/assets/mas/child.plist ./release/mas
 cp ../Internal-infrastructure/deployments/prod/Galleon/assets/mas/loginhelper.plist ./release/mas
 cp ../Internal-infrastructure/deployments/prod/Galleon/assets/mas/parent.plist ./release/mas
