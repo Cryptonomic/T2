2216ea3568f240c4822e41691cfe6adc9b5452da
Siddharth Vaderaa <svaderaa@gmail.com>
2022-02-07 19:39:42 +0100

    Fix: useCustomFee flag for choosing sendOperation
    Earlier commit was erroneously using the value of customFee === '0'
    to choose what values to call sendoperation with, this has been replaced
    by useCustomFee  attached to the state of the button toggle.

--- src/featureModals/Beacon/BeaconAuthorization.tsx
+++ src/featureModals/Beacon/BeaconAuthorization.tsx
@@ -125,12 +125,11 @@ const BeaconAuthorize = ({ open, managerBalance, onClose }: Props) => {
             if (isContract || isDelegation || isOrigination) {
                 // TODO: errors from here don't always bubble up
 
-                const operationResult =
-                    customFee === '0'
-                        ? await dispatch(sendOperations(password, operationDetails, utezFee, estimates.estimatedGas, estimates.estimatedStorage))
-                        : await dispatch(
-                              sendOperations(password, operationDetails, tezToUtez(customFee), parseInt(customGasLimit, 10), parseInt(customStorageLimit, 10))
-                          );
+                const operationResult = !useCustomFee
+                    ? await dispatch(sendOperations(password, operationDetails, utezFee, estimates.estimatedGas, estimates.estimatedStorage))
+                    : await dispatch(
+                          sendOperations(password, operationDetails, tezToUtez(customFee), parseInt(customGasLimit, 10), parseInt(customStorageLimit, 10))
+                      );
 
                 if (!!operationResult) {
                     setLedgerModalOpen(false);
