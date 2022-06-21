7c22fc993d50592d961fe6e7820df4f744390966
dev0623 <developer0623@gmail.com>
2022-06-22 04:49:09 +0900

    removed the nft gallery

--- src/components/AddressBlock/index.tsx
+++ src/components/AddressBlock/index.tsx
@@ -224,12 +224,7 @@ function AddressBlock(props: Props) {
     };
 
     function goToAccount(addressId, index, addressType, tokenName = '') {
-        const isMac = platform.indexOf('Mac') === 0;
-        if (isMac && addressType === AddressType.NFTGallery && isAppStore) {
-            setIsModalOpen(true, 'DisableNftModal');
-        } else {
-            dispatch(changeAccountThunk(addressId, publicKeyHash, index, identityIndex, addressType, tokenName));
-        }
+        dispatch(changeAccountThunk(addressId, publicKeyHash, index, identityIndex, addressType, tokenName));
     }
 
     function setIsModalOpen(open, active) {
@@ -325,10 +320,6 @@ function AddressBlock(props: Props) {
                     onClick={() => goToAccount(publicKeyHash, 0, AddressType.Manager)}
                 />
             )}
-
-            <AddDelegateLabel isActive={!isModalOpen && isNFTGallerysPageActive} onClick={() => goToAccount(publicKeyHash, 0, AddressType.NFTGallery)}>
-                <DelegateTitle>{t('general.nouns.nft_gallery')}</DelegateTitle>
-            </AddDelegateLabel>
 
             <AddDelegateLabel isActive={!isModalOpen && isLiquidityPageActive} onClick={() => goToAccount(publicKeyHash, 0, AddressType.PlatformLiquidity)}>
                 <DelegateTitle>{t('general.nouns.platform_liquidity')}</DelegateTitle>
